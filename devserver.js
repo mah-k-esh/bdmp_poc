a8c91cab5e7ae7e69b76ed60f4068e1dd59601ca
Makesh Kumar Murugesan <mahesh.mmk48@gmail.com>
2018-07-24 14:48:33 +0530

    port fix

--- devserver.js
+++ devserver.js
@@ -408,11 +408,4 @@ app.get('/dialogFlow',function(req,res){
 
 });
 
-app.listen(port, '0.0.0.0', function (err) {
-	if (err) {
-		console.log(err);
-		return;
-	}
-
-	console.log('Listening at http://0.0.0.0:%s', port);
-});
+app.listen(process.env.PORT || port);
